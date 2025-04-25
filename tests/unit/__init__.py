1d7f1975db75febcb88309a6dcca9b20e8f1fb9c
Adrià Cabello <adrian.cabello@alumni.esci.upf.edu>
2025-04-25 17:51:29 +0200

    added tests, mod toml

---
+++ tests/unit/__init__.py
@@ -0,0 +1,1 @@
+"""Unit tests package for sentiment analysis microservice.""" 
---
+++ tests/unit/test_import.py
@@ -0,0 +1,7 @@
+"""Unit tests for package import and version."""
+
+from sentiment_analysis import __version__
+
+def test_package_version():
+    """Test that the package version is correctly set."""
+    assert __version__ == "0.1.0" 
