7dd795fbe39a4b3172baca44a79a75f717aa8805
Adrià Cabello <adrian.cabello@alumni.esci.upf.edu>
2025-04-25 17:42:53 +0200

    inited project uv, and main directories

---
+++ src/sentiment_analysis/__init__.py
@@ -0,0 +1,7 @@
+"""
+Sentiment Analysis Microservice for Feddit.
+
+This package provides sentiment analysis functionality for comments in Feddit subfeddits.
+"""
+
+__version__ = "0.1.0" 
---
+++ src/sentiment_analysis/models/__init__.py
@@ -0,0 +1,1 @@
+"""Models package for the sentiment analysis microservice.""" 
